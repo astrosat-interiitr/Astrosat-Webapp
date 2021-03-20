1592a25998a8e3fe926745952982c974060ad48b
Rishabh Mehta <rmehta@ph.iitr.ac.in>
2021-03-20 19:22:00 +0530

    started react app

---
+++ frontend/cosmoscope/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router className="App">
+     <div className='App'>
+       Lets get this done!
+     </div>
+    </Router>
+  );
+}
+
+export default App;
