0d88e4d202afbcf3308eed7dcbf2b68963979a34
Rishabh Mehta <rmehta@ph.iitr.ac.in>
2021-03-21 04:21:30 +0530

    template with d3

--- frontend/cosmoscope/src/App.js
+++ frontend/cosmoscope/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import Canvas from './components/Canvas.js'
+import WorldMap from './components/worldmap'
 
 function App() {
   return (
     <Router className="App">
      <div className='App'>
-       Lets get this done!
+      <Switch>
+          <Route exact path='/' component={WorldMap} />
+        </Switch> 
      </div>
     </Router>
   );
