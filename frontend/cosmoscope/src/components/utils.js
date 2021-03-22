57235661038a5579832cbf8764be05b6b1eb7ef9
Rishabh Mehta <rmehta@ph.iitr.ac.in>
2021-03-22 07:15:27 +0530

    tried versor dragging

--- frontend/cosmoscope/src/components/utils.js
+++ frontend/cosmoscope/src/components/utils.js
@@ -8,3 +8,19 @@ export const projection = geoAitoff()
 export const graticule = geoGraticule10()
 
 export const outline = ({type: "Sphere"})
+
+export const getMwbackground = (d) => {
+  // geoJson object to darken the mw-outside, prevent greying of whole map in some orientations 
+  var res = {'type': 'FeatureCollection', 'features': [ {'type': 'Feature', 
+              'geometry': { 'type': 'MultiPolygon', 'coordinates' : [] }
+            }]};
+
+  // reverse the polygons, inside -> outside
+  var l1 = d.features[0].geometry.coordinates[0];
+  res.features[0].geometry.coordinates[0] = [];
+  for (var i=0; i<l1.length; i++) {
+    res.features[0].geometry.coordinates[0][i] = l1[i].slice().reverse();
+  }
+
+  return res;
+}
