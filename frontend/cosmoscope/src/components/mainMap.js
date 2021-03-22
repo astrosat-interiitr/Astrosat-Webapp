dfea152a49adf89f2bf3152d2d3f6c4601c8064b
Rohit Jha <rjha@ph.iitr.ac.in>
2021-03-23 02:28:23 +0530

    feat: d3 drag test

--- frontend/cosmoscope/src/components/mainMap.js
+++ frontend/cosmoscope/src/components/mainMap.js
@@ -17,69 +17,39 @@ function Map(props){
   // const [path, setPath] = useState(initPath);
     // const [projection, setProjection] = useState(initProj);
   // const [graticule, setGraticule] = useState(graticule);
-  
+  var v0;
+  var r0;
+  var q0;
 
   useEffect(() => {
-    let map = d3.select("svg")
-    map
-    .call(drag(projection))
+    let map = d3.select("svg") 
+    
+    map.call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    );
   })
 
-  const drag = (projection) => {
-    let v0, q0, r0, a0, l;
+  const dragstarted = (event) => {
+    v0 = versor.cartesian(projection.invert(d3.pointer(event)));
+    r0 = projection.rotate();
+    q0 = versor(r0);
 
-    function pointer(event, that) {
-      const t = d3.pointers(event, that);
-      if (t.length !== l) {
-        l = t.length;
-        if (l > 1) a0 = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
-        dragstarted.apply(that, [event, that]);
-      }
-  
-      // For multitouch, average positions and compute rotation.
-      if (l > 1) {
-        const x = d3.mean(t, p => p[0]);
-        const y = d3.mean(t, p => p[1]);
-        const a = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
-        return [x, y, a];
-      }
-  
-      return t[0];
-    }
-    
-    const dragstarted = (e) => {
-      v0 = versor.cartesian(projection.invert(pointer()));
-      q0 = versor(r0 = projection.rotate());
-    }
-
-    const dragged = (e) => {
-
-      const p = pointer();
-      const v1 = versor.cartesian(projection.rotate(r0).invert(p));
-      const delta = versor.delta(v0, v1);
-      let q1 = versor.multiply(q0, delta);
-  
-      // For multitouch, compose with a rotation around the axis.
-      if (p[2]) {
-        const d = (p[2] - a0) / 2;
-        const s = -Math.sin(d);
-        const c = Math.sign(Math.cos(d));
-        q1 = versor.multiply([Math.sqrt(1 - s * s), 0, 0, c * s], q1);
-      }
-      
-      const newPath = geoPath(projection.rotate(versor.rotation(q1)))
-      
-      console.log(newPath)
-  
-      // In vicinity of the antipode (unstable) of q0, restart.
-      if (delta[0] < 0.7) dragstarted.apply(this, [e, this]);
-    }
-
-    return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged);
+    console.log("here")
   }
   
+  const dragged = (event) => {
+    var v1 = versor.cartesian(projection.rotate(r0).invert(d3.pointer(event))),
+        q1 = versor.multiply(q0, versor.delta(v0, v1)),
+        r1 = versor.rotation(q1);
+    projection.rotate(r1);
+
+    console.log("hereff")
+    
+  }
+
+
+ 
   return (
     <div>
       <svg 
